9b7e6aa3710624e8d2957787e0b26bd14dfa50c6
Tosi-eu <galdiniguilherme@usp.br>
2025-12-21 12:09:35 -0300

    refactor: renamed files

---
+++ src/infrastructure/web/routes/notificacao.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { NotificationEventService } from '../../../core/services/notificacao.service';
+import { NotificationEventController } from '../controllers/notificacao.controller';
+import { NotificationEventRepository } from '../../database/repositories/notificacao.repository';
+
+const repo = new NotificationEventRepository();
+const service = new NotificationEventService(repo);
+const controller = new NotificationEventController(service);
+
+const router = Router();
+
+router.post('/', (req, res) => controller.create(req, res));
+router.get('/', (req, res) => controller.getAll(req, res));
+router.get('/retirar-hoje', (req, res) => controller.getToday(req, res));
+router.get('/:id', (req, res) => controller.getById(req, res));
+router.patch('/:id', (req, res) => controller.update(req, res));
+router.delete('/:id', (req, res) => controller.delete(req, res));
+
+export default router;
